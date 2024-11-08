29f2f108152f9ef625060c78c6f97f38f24057b5
Rob Harting <robharting@hotmail.com>
2024-11-08 11:07:55 +0100

    create box in AR

--- ARDicee-iOS/ViewController.swift
+++ ARDicee-iOS/ViewController.swift
@@ -19,22 +19,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // chamferRadius is how rounded you want the corners of your box to be. Size is in meter
+        let cube  = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        
+        print("ARWorldTrackingConfiguration.isSupported: \(ARWorldTrackingConfiguration.isSupported)")
         if ARWorldTrackingConfiguration.isSupported {
             
             // Create a session configuration
             let configuration = ARWorldTrackingConfiguration()
-            
-            print("ARWorldTrackingConfiguration.isSupported: \(ARWorldTrackingConfiguration.isSupported)")
             
             // Run the view's session
             sceneView.session.run(configuration)
